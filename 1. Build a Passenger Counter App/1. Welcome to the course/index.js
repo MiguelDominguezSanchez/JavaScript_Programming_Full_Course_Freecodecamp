55bfe30f27cb5069dab479e6efe36611c5c6b024
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-07-28 09:40:23 +0200

    Create variable

--- 1. Build a Passenger Counter App/1. Welcome to the course/index.js
+++ 1. Build a Passenger Counter App/1. Welcome to the course/index.js
@@ -1,1 +1,15 @@
 // javascript
+
+// document.getElementById('count-el').innerText = 5
+
+let count = 0
+
+console.log(count)
+
+console.log(myAge)
+
+// 1. Create a variable, myAge, and set its value to your age
+let myAge = 35
+
+// 2. Log the myAge variable to the console
+// console.log(myAge)/

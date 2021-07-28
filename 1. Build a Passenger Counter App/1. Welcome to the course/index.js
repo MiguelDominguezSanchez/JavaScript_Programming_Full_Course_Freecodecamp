8149ec02e12a784dbc7e560f02262c9eb070d7a2
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-07-28 13:27:02 +0200

    Reassigning & incrementing

--- 1. Build a Passenger Counter App/1. Welcome to the course/index.js
+++ 1. Build a Passenger Counter App/1. Welcome to the course/index.js
@@ -37,6 +37,7 @@
 // 2. Multiply the two together and store the result in myDogAge
 // 3 Log myDogAge to the console
 
+/*
 let myAge = 35
 
 let humanDogRatio = 7
@@ -44,3 +45,61 @@ let humanDogRatio = 7
 let myDogAge = myAge * humanDogRatio
 
 console.log(myDogAge)
+*/
+
+let count = 5
+
+// count + 1
+// count = 3
+
+// count = 1
+
+// count = count + 1 // 5 + 1 = 6
+
+/*
+count = count + 2 // 5 + 2 = 7
+
+count = count + 10
+
+count = count - 2
+
+console.log(count)
+*/
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+// Decrease it down to 25, and then finally  increase it to 70
+// Console.log the value for each step
+
+/* my solution
+
+let bonusPoints = 50
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints * 2
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints / 4
+
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+
+console.log(bonusPoints)
+*/
+
+/* teacher solution
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+*/
